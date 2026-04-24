da59ca82bb22a362dc7989b04decbde070435f5e
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-04-24 11:30:10 +0000

    fix mistakes (missing noexcept removing val init value)

--- include/cpp_abstractions_lab/optional.hpp
+++ include/cpp_abstractions_lab/optional.hpp
@@ -10,24 +10,26 @@ inline constexpr nullopt_t nullopt{0};
 //TODO template<typename T>
 class Optional{
 private:
-    int val = 0;
+    int val;
     bool has_val;
 public:
     Optional() : has_val(false) {}
     Optional(nullopt_t) : has_val(false) {}
     Optional(int val) : has_val(true), val(val) {}
     Optional(const Optional& other);
-    Optional(Optional&& other);
+    Optional(Optional&& other) noexcept;
 
     Optional& operator=(const Optional& other) = default;
     Optional& operator=(Optional&& other) = default;
 
     bool operator==(const Optional& other) const;
     bool operator==(int other) const;
+    bool operator!=(const Optional& other) const;
+    bool operator!=(int other) const;
 
     bool has_value() const;
     int value() const;
     int value_or(int default_value) const;
 
-    void reset() noexcept {has_val = false; val=0;}
+    void reset() noexcept {has_val = false;}
 };
--- src/optional.cpp
+++ src/optional.cpp
@@ -4,33 +4,36 @@
 #include <utility>
 
 Optional::Optional(const Optional& other): has_val(other.has_val), val(other.val) {}
-Optional::Optional(Optional&& other): has_val(std::move(other.has_val)), val(std::move(other.val)) {}
+Optional::Optional(Optional&& other) noexcept: has_val(std::move(other.has_val)), val(std::move(other.val)) {}
 
 bool Optional::operator==(const Optional& other) const{
     if(has_val != other.has_val)
         return false;
 
-    if(false == has_val)
+    if(!has_val)
         return true;
 
     return (val == other.val);
 }
 bool Optional::operator==(int other) const{
-    if(false == has_val)
+    if(!has_val)
         return false;
 
     return (val == other);
 }
 
+bool Optional::operator!=(const Optional& other) const {return !(*this==other);}
+bool Optional::operator!=(int other) const {return !(*this==other);};
+
 bool Optional::has_value() const {return has_val;}
 int Optional::value() const {
-    if(false == has_val)
+    if(!has_val)
         throw std::bad_optional_access();
     
     return val;
 }
 int Optional::value_or(int default_value) const{
-    if(false == has_val)
+    if(!has_val)
         return default_value;
     
     return val;
