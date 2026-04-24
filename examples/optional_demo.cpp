64aa78b0676e03bce0440ff252f8029069b3031d
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-04-24 10:33:58 +0000

    add optional  implementation and demo

--- examples/optional_demo.cpp
+++ examples/optional_demo.cpp
@@ -1,5 +1,36 @@
 #include "cpp_abstractions_lab/optional.hpp"
+#include <optional>
+#include <iostream>
 
 int main(){
+    Optional x;
+    try{
+        int z = x.value();
+    }
+    catch(const std::bad_optional_access& e){
+        std::cout << e.what() << "\n";
+    }
+    
+    std::cout << "x has value? " << x.has_value() << "\n";
+    std::cout << "x's value is 4? " << (x == 4) << "\n";
+    std::cout << "x's value is nullopt? " << (x == nullopt) << "\n";
+    std::cout << "x's value is? or 42? " << x.value_or(42) << "\n\n";
+
+    x = 4;
+    std::cout << "x has value? " << x.has_value() << "\n";
+    std::cout << "x's value is 4? " << (x == 4) << "\n";
+    std::cout << "x's value is nullopt? " << (x == nullopt) << "\n";
+    std::cout << "x's value is? or 42? " << x.value_or(42) << "\n";
+    std::cout << "x's value is? " << x.value() << "\n\n";
+
+    x = nullopt;
+    std::cout << "x has value? " << x.has_value() << "\n";
+    std::cout << "x's value is 4? " << (x == 4) << "\n";
+    std::cout << "x's value is nullopt? " << (x == nullopt) << "\n\n";
+
+    x = 42;
+    std::cout << "x has value? " << x.has_value() << "\n";
+    x.reset();
+    std::cout << "x has value? " << x.has_value() << "\n";
     return 0;
 }
---
+++ include/cpp_abstractions_lab/optional.hpp
@@ -0,0 +1,33 @@
+#pragma once
+
+
+struct nullopt_t{
+    constexpr explicit nullopt_t(int) {};
+};
+
+inline constexpr nullopt_t nullopt{0};
+
+//TODO template<typename T>
+class Optional{
+private:
+    int val = 0;
+    bool has_val;
+public:
+    Optional() : has_val(false) {}
+    Optional(nullopt_t) : has_val(false) {}
+    Optional(int val) : has_val(true), val(val) {}
+    Optional(const Optional& other);
+    Optional(Optional&& other);
+
+    Optional& operator=(const Optional& other) = default;
+    Optional& operator=(Optional&& other) = default;
+
+    bool operator==(const Optional& other) const;
+    bool operator==(int other) const;
+
+    bool has_value() const;
+    int value() const;
+    int value_or(int default_value) const;
+
+    void reset() noexcept {has_val = false; val=0;}
+};
