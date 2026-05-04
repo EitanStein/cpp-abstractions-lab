f311a6508de0f6b74a8587fd2778ad105fb2fe37
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-05-04 21:02:06 +0000

    add function implementation

---
+++ examples/function_demo.cpp
@@ -0,0 +1,45 @@
+#include "cpp_abstractions_lab/function.hpp"
+#include <iostream>
+
+int myIntFunc(int x){
+    return x*2;
+}
+
+void myVoidFunc(){
+    std::cout << "my Void func\n";
+}
+
+int main(){
+    {
+        Function<int(int)> func(&myIntFunc);
+        std::cout << func(2) << "\n";
+    }
+    {
+        Function<void()> func(&myVoidFunc);
+        func();
+    }
+    {
+        Function<int(int,int)> func{[](int x,int y) -> int{ return x+y;}};
+        std::cout << func(2,5) << "\n";
+    }
+    {
+        Function<int(int,int)> func{[](int x,int y) -> int{ return x+y;}};
+        func = {[](int x,int y) -> int{ return x*y;}};
+        std::cout << func(2,5) << "\n";
+    }
+    {
+        Function<int(int,int)> func1;
+        Function<int(int,int)> func2{[](int x,int y) -> int{ return x+y;}};
+        func1 = func2;
+        std::cout << func1(2,5) << "\n";
+        std::cout << func2(2,7) << "\n";
+    }
+    {
+        Function<int(int,int)> func1;
+        Function<int(int,int)> func2{[](int x,int y) -> int{ return x+y;}};
+        func1 = std::move(func2);
+        std::cout << func1(2,5) << "\n";
+        std::cout << func2(2,7) << "\n";
+    }
+    return 0;
+}
---
+++ include/cpp_abstractions_lab/function.hpp
@@ -0,0 +1,89 @@
+#pragma once
+#include <utility>
+
+
+
+template<typename>
+class Function;
+
+template<typename R, typename... Args>
+class Function<R(Args...)> {
+private:
+    void* object;
+    R(*invoke)(void*, Args&&...);
+    void(*destroy)(void*);
+    void*(*copy)(void*);
+
+    template<typename Func>
+    static R invoke_fn(void* obj, Args&&... args){
+        return (*static_cast<Func*>(obj))(std::forward<Args>(args)...);
+    }
+
+    template<typename Func>
+    static void delete_fn(void* obj){
+        delete static_cast<Func*>(obj);
+    }
+
+    template<typename Func>
+    static void* copy_fn(void* obj){
+        return new Func(*static_cast<Func*>(obj));
+    }
+
+
+public:
+    Function() { object = nullptr; }
+
+    // TODO add &&?
+    template<typename Func>
+    Function(Func func) {
+        object = new Func(std::move(func));
+        invoke = &invoke_fn<Func>;
+        destroy = &delete_fn<Func>;
+        copy = &copy_fn<Func>;
+    }
+    ~Function() {
+        destroy(object);
+    }
+
+    R operator()(Args... args){
+        return invoke(object, std::forward<Args>(args)...);
+    }
+
+    Function& operator=(const Function& other){
+        destroy(object);
+
+        object = copy(other.object);
+
+        invoke = other.invoke;
+        destroy = other.destroy;
+        copy = other.copy;
+
+        return *this;
+    }
+
+    Function& operator=(Function&& other){
+        destroy(object);
+
+        object = std::move(other.object);
+        other.object = nullptr;
+
+        invoke = other.invoke;
+        destroy = other.destroy;
+        copy = other.copy;
+
+        return *this;
+    }
+
+    // TODO add &&?
+    template<typename Func>
+    Function& operator=(Func func){
+        destroy(object);
+
+        object = new Func(std::move(func));
+        invoke = &invoke_fn<Func>;
+        destroy = &delete_fn<Func>;
+        copy = &copy_fn<Func>;
+
+        return *this;
+    }
+};
