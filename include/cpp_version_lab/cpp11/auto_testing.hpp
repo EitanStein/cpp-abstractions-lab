a29af0cdb663dcd0c6e37a8314d6b67dfcb07198
Eitan Stein <31282300+EitanStein@users.noreply.github.com>
2026-07-02 09:10:59 +0000

    add cpp versions lab with some cpp11 auto testing

---
+++ include/cpp_version_lab/cpp11/auto_testing.hpp
@@ -0,0 +1,53 @@
+#pragma once
+#include <print>
+#include <vector>
+#include <string>
+#include <string_view>
+
+template<typename T>
+void print_type(){
+    std::string_view name = __PRETTY_FUNCTION__;
+    size_t start = name.find("T = ") + 4;
+    size_t end = name.find("]");
+    println("{}", name.substr(start, end-start));
+}
+
+void testing_auto_deduction(){
+    {
+        auto x = 5;
+        print_type<decltype(x)>();   // int
+    }
+    {
+        auto x = 5.0;
+        print_type<decltype(x)>(); // double
+    }
+    {
+        auto x = 5.0f;
+        print_type<decltype(x)>(); // float
+    }
+    {
+        auto x = true;
+        print_type<decltype(x)>(); // bool
+    }
+    {
+        auto x = 0;
+        print_type<decltype(x)>(); // int
+    }
+    {
+        auto x = std::vector<int>{1,2,3};
+        print_type<decltype(x)>(); // std::vector<int>
+    }
+    {
+        auto x = std::vector<double>{1,2,3};
+        print_type<decltype(x)>(); // std::vector<double>
+    }
+    {
+        auto x = [](int x){return "my lambda";};
+        print_type<decltype(x)>();      // testing_auto_deduction()::<lambda(int)>
+    }
+
+    // std::vector<auto> x{1,2,3} - compile error can't use auto inside <> 
+
+    // auto in return type of func not allowed in C++11 - added in a later version
+    // auto func();
+}
---
+++ version_testing/cpp11.cpp
@@ -0,0 +1,6 @@
+#include "cpp_version_lab/cpp11/auto_testing.hpp"
+
+int main(){
+    testing_auto_deduction();
+    return 0;
+}
